06ae920c6302d72635399988aaed033e202c1d7b
rahulsha01 <make.rahulsharma@gmail.com>
2024-06-25 18:17:07 +0530

    order Model added

---
+++ models/OrderModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+var Orders = new Schema({
+     category_id  : { type: String, required: true, max: 36  },
+     category_name : { type: String, required: true, max: 30  },
+     category_desc : { type: String, required: true, max: 300 },
+     created_at : { type: Date },
+     updated_at : { type: Date }
+});
+
+// Export the model
+module.exports = mongoose.model("orders", Orders);
