e029e996faf041624fb04b565d5fadb477c501ac
rahulsha01 <make.rahulsharma@gmail.com>
2024-06-25 21:30:57 +0530

    mongoose contanst added

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -1,5 +1,4 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+const {Schema } = require('../utility/constant')
 
 let Product = new Schema({
   productName: { type: String, required: true, max: 30 },
@@ -8,10 +7,15 @@ let Product = new Schema({
   procutPrice: { type: Number, required: true, max: 100  },
   productStockQuantity: { type: String, required: true, max: 100 },
   category_id: { type: String, required: true, max: 100 },
-  productImagesUrl: { type: String, required: true, max: 100 },
+  productImagesUrl: { type: IMG, required: true, max: 100 },
   productCreatedAt: { type: Date },
   procutModifiedAt: { type: Date },
 });
+
+const IMG = {
+    url: { type: String, required: true, max: 300 },
+    size: { type: String, required: true, max: 300 }
+}
 
 // Export the model
 module.exports = mongoose.model("Products", Product);
